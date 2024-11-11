cc0a2289141e2e7c34ff0b8d6afac0872f69d8b3
Pavel <mankov.174@gmail.com>
2024-11-11 22:33:03 +0500

    Changed data type subscription

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,6 +11,6 @@ public class Main {
         post.patronymic = "Иванович";
         post.phone = "+7 (999)-999-99-99";
         post.surname = "Иванов";
-        post.subscription = String.valueOf(true);
+        post.subscription = true;
     }
 }
